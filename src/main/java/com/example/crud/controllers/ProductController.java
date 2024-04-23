b55ab56aaa0ed04f1d0715ec6c97c5ece89cb525
roger <roger.lavarda@ibiruba.ifrs.edu.br>
2024-04-23 17:48:46 -0300

    Initial commit

---
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -0,0 +1,60 @@
+package com.example.crud.controllers;
+
+import com.example.crud.domain.product.Product;
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController{
+    @Autowired
+    private ProductRepository repository;
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        var allProducts = repository.findAllByActiveTrue();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){
+        //Product product = repository.getReferenceById(data.id());
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Product product = optionalProduct.get();
+        product.setName(data.name());
+        product.setPrice_in_cents(data.price_in_cents());
+        //repository.save(product);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        Optional<Product> optionalProduct = repository.findById(id);
+        if(optionalProduct.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Product product = optionalProduct.get();
+        product.setActive(false);
+        return ResponseEntity.noContent().build();
+
+    }
+}
